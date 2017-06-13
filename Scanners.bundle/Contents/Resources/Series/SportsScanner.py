e6bf616a9c3141ac3e18050cd2fc9761295bd71d
Alon Albert <alon.albert@gmail.com>
2017-06-13 05:51:27 +0300

    Some F1 tweaks

--- Scanners.bundle/Contents/Resources/Series/SportsScanner.py
+++ Scanners.bundle/Contents/Resources/Series/SportsScanner.py
@@ -109,8 +109,8 @@ class F1Handler(RegexHandler):
 
   def getRegexs(self):
     return [
-      '^Formula.?1.*',
-      '^F1.*',
+      'Formula.?1.*',
+      'F1.*',
     ]
 
   def handle(self, match, file):
